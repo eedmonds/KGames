961a02036e79ef7ae60a3ffc34eacb38c804b243
eedmonds <ericedmonds@icloud.com>
2022-05-03 14:43:38 -0400

    Initial Commit

---
+++ KGames/KGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  KGamesApp.swift
+//  KGames
+//
+//  Created by Eric Edmonds on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct KGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
